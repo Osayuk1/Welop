9fa1ad6253da35cbdaf932e2759af457493d4dd2
Osayuk1 <157780677+Osayuk1@users.noreply.github.com>
2024-08-23 01:43:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // script.js
 
-let cart = [];
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 function addToCart(itemName, itemPrice) {
     cart.push({ name: itemName, price: itemPrice });
+    localStorage.setItem('cart', JSON.stringify(cart));
     alert(`${itemName} has been added to your cart!`);
     console.log(cart);
+}
+
+// Function to view the cart page
+function viewCart() {
+    window.location.href = "cart.html";
 }
